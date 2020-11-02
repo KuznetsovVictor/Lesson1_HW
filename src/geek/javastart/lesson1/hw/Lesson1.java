70e239a5819117f0c938c14a339a455f63c2d92e
VictorKuznetsov88 <bitv2010@yandex.ru>
2020-11-02 13:28:58 +0300

    Lesson1_201031

--- src/geek/javastart/lesson1/hw/Lesson1.java
+++ src/geek/javastart/lesson1/hw/Lesson1.java
@@ -1,8 +1,9 @@
 package geek.javastart.lesson1.hw;
 
+
 public class Lesson1 {
 
-    /* Написать метод вычисляющий выражение a * (b + (c / d))
+    /* 1) Написать метод вычисляющий выражение a * (b + (c / d))
       и возвращающий результат с плавающей точкой,
       где a, b, c, d – целочисленные входные параметры этого метода */
 
@@ -11,79 +12,86 @@ public class Lesson1 {
     }
 
 
-    /*    Написать метод, принимающий на вход два целых числа,
+    /*    2) Написать метод, принимающий на вход два целых числа,
      и проверяющий что их сумма лежит в пределах от 10 до 20(включительно),
     если да – вернуть true, в противном случае – false; */
 
-    public static boolean limitVerifying(int a, int b){
+    public static boolean limitVerifying(int a, int b) {
         int sum = a + b;
-        if((sum >= 10) && (sum <= 20 )){
+        if ((sum >= 10) && (sum <= 20)) {
             return true;
-        }else
+        }
             return false;
     }
 
-  /*  Написать метод, которому в качестве параметра передается целое число,
+  /*  3) Написать метод, которому в качестве параметра передается целое число,
    метод должен проверить положительное ли число передали, или отрицательное.
    Замечание: ноль считаем положительным числом. Результат работы метода вывести в консоль */
 
-  public static void numberCheck(int a){
+  public static Object numberCheck(int a){
       if (a >= 0){
-          System.out.println("Число " + a + " положительное." );
+          return ("Число " + a + " положительное." );
       }else {
-          System.out.println("Число " + a + " отрицательное." );
+          return ("Число " + a + " отрицательное." );
       }
   }
 
-    /*   Написать метод, которому в качестве параметра передается строка,
+    /*   4) Написать метод, которому в качестве параметра передается строка,
         обозначающая имя, метод должен вернуть приветственное сообщение
         «Привет, переданное_имя!»; Вывести приветствие в консоль.*/
     public static String welcome(String name){
         return "Привет " + name + "!";
     }
 
- /*   Написать метод, который определяет является ли год високосным.
+ /*  5)  Написать метод, который определяет является ли год високосным.
         Каждый 4-й год является високосным,
         кроме каждого 100-го,
         при этом каждый 400-й – високосный.
         Для проверки работы вывести результаты работы метода в консоль*/
 
-    public static void leapYear(int year){
+    public static Object leapYear(int year){
         if(((year % 4 == 0) || (year % 400 == 0)) && (year % 100 != 0)){
-            System.out.println(year + " високосный!");
+            return (year + " високосный!");
         }else{
-            System.out.println(year + " не високосный!");
+            return (year + " не високосный!");
         }
     }
 
 
     public static void main(String[] args) {
-        double a,b,c,d;
+
+        System.out.println("Задание № 1.");
+        int a,b,c,d;
         a = 1;
         b = 12;
         c = 32;
         d = 6;
         double result = computedExpressions(a,b,c,d);
-        System.out.println(result);
+        System.out.println(result + "\n");
 
+        System.out.println("Задание № 2.");
         int arg1,arg2;
-        arg1 = 12;
+        arg1 = 6;
         arg2 = 8;
         boolean ret = limitVerifying(arg1,arg2);
-        System.out.println(ret);
+        System.out.println(ret + "\n");
 
+        System.out.println("Задание № 3.");
         int numCheck;
-        numCheck = -2;
-        numberCheck(numCheck);
+        numCheck = 8;
+        System.out.println(numberCheck(numCheck) + "\n");
 
+        System.out.println("Задание № 4.");
         String name;
         name = "Иван";
         String returnWelcom = welcome(name);
-        System.out.println(returnWelcom);
+        System.out.println(returnWelcom + "\n");
 
+        System.out.println("Задание № 5.");
         int year;
         year = 2020;
-        leapYear(year);
+        System.out.println(leapYear(year) + "\n");
+
 
     }
 
